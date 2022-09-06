b7856e19808ed80495c63e93e665f182ea655c0d
iandhyan <112957343+iandhyan@users.noreply.github.com>
2022-09-06 10:22:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,16 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
+
 # Fruit Picker
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.set_index('Fruit'))
 
-#Display the table on page
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#Display the table on page                      
 streamlit.dataframe(my_fruit_list)
-
-
-
