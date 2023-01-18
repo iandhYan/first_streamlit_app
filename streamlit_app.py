a52d04272d6ef0fb67f994a3cafc7f04ecbfaa22
iandhyan <112957343+iandhyan@users.noreply.github.com>
2023-01-18 14:29:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ streamlit.dataframe(fruits_to_show)
 def get_fruityvice_data(this_fruit_choice):
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+        return fruityvice_normalized
 
 #New section to display frutyvice api response
 streamlit.header("Fruityvice Fruit Advice!")
@@ -64,7 +64,7 @@ streamlit.header("The fruit load list contains")
     def get_fruit_load_list():
             with my_cur = my_cnx.cursor() as my_cur
             my_cur.execute("select * from fruit_load_list")
-       return my_cur.fetchall()
+            return my_cur.fetchall()
 
 
 #allow user to add fruits to the list
